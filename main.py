7f40be1d8d7d29fb7d64ae228c83e5e81629eb36
shadow0911 <yoyofahimmonaemkhandj@gmail.com>
2022-09-08 01:23:26 +0600

    Added player death

--- main.py
+++ main.py
@@ -1,58 +1,87 @@
 import json
 import random
+import time
 
 class Player(object):
     def __init__(self):
-        with open("./player.json", "r") as f:
+        with open("./db/player.json", "r") as f:
             self.playerData = json.load(f)
 
-        self.profile = self.playerData['profile']
+        self.player = self.playerData['profile']
         self.sword = None
-    def _profile(self):
-        pass
 
     def _inventory(self):
-        with open("./swords.json", "r") as f:
+        with open("./db/swords.json", "r") as f:
             self.inventory = json.load(f)
 
-        self.sword = self.inventory[self.profile['sword']]
+        self.sword = self.inventory[self.player['equepments']['sword']]
+
+    def _Profile(self):
+        print(f"Rank: {self.player['rank']['title']} \nLevel {self.player['rank']['level']}")
 
 class Adventure(Player):
     def __init__(self):
         super().__init__()
 
-        with open("./enemy.json", "r") as f:
+        with open("./db/enemy.json", "r") as f:
             self.monsterData = json.load(f)
 
-        self.monster = self.monsterData['Eastborne'][0]['slime']
-        self.inBattle = True
+        self.monster = self.monsterData['Eastborne'][0]
+        self.inBattle = False
 
-    def _battle(self):
-        if self.monster['hitpoint'] == 0:
+    def _AdvStart(self):
+        print("You went on a great adventure...")
+        time.sleep(2)
+
+        print(f"You encounted a {self.monster['name']}")
+        if self.inBattle == False:
+            self.inBattle = True
+
+    def _Battle(self):
+        self._Attack()
+
+        if int(self.player['hitpoint']) <= 0:
             self.inBattle = False
-            print("You won")
-        else:
-            dmg = random.randint(int(self.monster['damageMin']), int(self.monster['damageMax']))
-            self.profile['hitpoint'] -= dmg
-            print(self.sword)
-            print(f"You received {dmg} damage and did {self.sword['damage']} damage")
-            self.monster['hitpoint'] -= self.sword['damage']
+            print("You Died")
+
+            returnTown = input("Press Enter to continue...")
+            if returnTown:
+                pass
+
+        if int(self.monster['hitpoint']) <= 0:
+            self.inBattle = False
+            print(f"{self.monster['name']} felted")
+
+    def _Attack(self):
+        dmg = random.randint(int(self.monster['damageMin']), int(self.monster['damageMax']))
+        self.player['hitpoint'] -= dmg
+
+        print(f"You received {dmg} damage and did {self.sword['damage']} damage")
+        self.monster['hitpoint'] -= self.sword['damage']
+
+class Map(object):
+    def __init__(self):
+        pass
 
 class Main(Adventure):
     def __init__(self):
         super().__init__()
         self._inventory()
 
-    def input(self, inp):
-        if inp == 'adventure':
-            self._battle()
+    def _MainFrame(self):
+        actionAdd = input('Type your action: ')
+        if actionAdd:
+            self._Actions(actionAdd)
+
+    def _Actions(self, inp):
+        if inp == 'adventure' or inp == 'adv':
+            self._AdvStart()
             while self.inBattle == True:
-                atkMode = input("Type 'atk': ")
+                atkMode = input("Type 'atk' to attack: ")
                 if atkMode:
-                    self._battle()
+                    self._Battle()
 
-inputDetector = input('Type your action: ')
-play = Main().input(inputDetector)
+        elif inp == 'profile':
+            self._Profile()
 
-
-
+play = Main()._MainFrame()
